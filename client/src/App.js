c9bf6094ab0205c8fe3f41f900efcf9c88339241
Alyssa Barrett <cicibarrett@gmail.com>
2022-09-15 14:10:52 +1200

    fixed the app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,19 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import QuickSortAlyssa from './components/QuickSortAlyssa';
-import QuickSortBernie from './components/QuickSortBernie';
-import QuickSortSonja from './components/QuickSortSonja';
+import QuickSortAlyssa from "./components/QuickSortAlyssa";
+import QuickSortBernie from "./components/QuickSortBernie";
+import QuickSortSonja from "./components/QuickSortSonja";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/Quick-Sort-Alyssa" element={QuickSortAlyssa}></Route>
-      <Route path="/Quick-Sort-Alyssa" element={QuickSortBernie}></Route>
-      <Route path="/Quick-Sort-Alyssa" element={QuickSortSonja}></Route>
-    </Routes>
-    </BrowserRouter>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Quick-Sort-Alyssa" element={QuickSortAlyssa}></Route>
+          <Route path="/Quick-Sort-Bernie" element={QuickSortBernie}></Route>
+          <Route path="/Quick-Sort-Sonja" element={QuickSortSonja}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
