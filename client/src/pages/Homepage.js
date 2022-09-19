764248cff9e87c62ad4c2f54938edd068ad375b0
Sonja Fantom <sonja.fantom@gmail.com>
2022-09-19 23:18:09 +1200

    added footer component and started homepage

---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import HeroImage from '../components/Homepage/HeroImage.js'
+import LatestReleases from '../components/Homepage/LatestReleases.js'
+import ShopByNeeds from '../components/Homepage/ShopByNeeds.js'
+import ShopByBrands from '../components/Homepage/ShopByBrands.js'
+import CustomerReviews from '../components/Homepage/CustomerReviews.js'
+import PickYourLaptop from '../components/Homepage/PickYourLaptop'
+
+const Homepage = () => {
+  return (
+    <>
+        <HeroImage />
+        <LatestReleases />
+        <ShopByNeeds />
+        <ShopByBrands />
+        <CustomerReviews />
+        <PickYourLaptop />
+        <Footer></Footer>
+    </>
+  )
+}
+
+export default Homepage
