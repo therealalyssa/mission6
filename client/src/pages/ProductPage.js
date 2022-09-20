8468ae7dff388d1170e1931099b819ced9fc1e75
Alyssa Barrett <cicibarrett@gmail.com>
2022-09-21 10:09:20 +1200

    quiz added

---
+++ client/src/pages/ProductPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Quiz from "../components/Quiz";
+
+const Homepage = () => {
+  return (
+    <>
+      <Quiz />
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default Homepage;
