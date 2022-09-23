89884dba76f5d8c241ca473dee3046b91f754e80
berniem <berniem@missionreadyhq.com>
2022-09-23 16:34:03 +1200

    small listing page change

--- client/src/pages/ListingPage.js
+++ client/src/pages/ListingPage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from '../components/Header/Header.js';
 import Layout from '../components/ListingPage/Layout.js';
-// import Footer from '../components/Footer';
+import Footer from '../components/Footer';
 
 function ListingPage() {
   return (
     <>
       <Header />
       <Layout />
-      {/* <Footer /> */}
+      <Footer />
     </>
   )
 }
